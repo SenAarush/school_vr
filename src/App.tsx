9c2c7420623a03a971a58e1cde454bc3f9fd7cb5
SenAarush <senaarush@outlook.com>
2024-03-09 22:58:56 +0530

    jus making a commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import Chemverse from "./pages/Chemverse/page"
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Chemverse/>}></Route>
-        {/* <Route path="/chemverse" element={<Chemverse />}></Route> */}
-      </Routes>
+      <Chemverse/>
     </>
   )
 }
